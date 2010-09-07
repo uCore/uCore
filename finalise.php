04a5317575881f1f13e83b6abfae9eb3c04fee13
oridan <oridan@13c52f6a-24c2-4fbb-bd14-5d0970e89a5a>
2010-09-07 19:45:22 +0000

    - end timers properly in finalise - improve svn status in admin

--- finalise.php
+++ finalise.php
@@ -5,8 +5,10 @@ $toggleT = 'Show Timers';
 if (array_key_exists('admin_showT',$_SESSION) && $_SESSION['admin_showT'] === true) {// || array_key_exists('showT',$_REQUEST) && array_key_exists('timers',$GLOBALS)) {
 	$toggleT = 'Hide Timers';
 	echo '<pre>';
-	foreach ($GLOBALS['timers'] as $name => $info)
-		echo nl2br($info['time_taken']."\t$name\n");
+	foreach ($GLOBALS['timers'] as $name => $info) {
+		$time = !array_key_exists('time_taken',$info) ? timer_end($name) : $info['time_taken'];
+		echo "$time\t\t$name\n";
+	}
 	echo '</pre>';
 }
 $toggleQ = 'Show Queries';
@@ -14,7 +16,7 @@ if (array_key_exists('admin_showQ',$_SESSION) && $_SESSION['admin_showQ'] === tr
 	$toggleQ = 'Hide Queries';
 	echo '<pre>';
 	foreach ($GLOBALS['sql_queries'] as $info)
-		echo nl2br(print_r($info,true)."\n\n");
+		echo print_r($info,true)."\n\n";
 	echo '</pre>';
 }
 
