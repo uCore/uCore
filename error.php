ff0eb462f4396fa734b93ef32bef7d6115e2b0ec
oridan <oridan@13c52f6a-24c2-4fbb-bd14-5d0970e89a5a>
2010-11-15 13:34:02 +0000

    add User Agent to DebugMail

--- error.php
+++ error.php
@@ -39,7 +39,8 @@ function EchoException($e) {
 function DebugMail($subject,$message) {
   if (!is_string($message)) $message = print_r($message,true);
   $ref = array_key_exists('HTTP_REFERER',$_SERVER) ? 'Referrer: '.$_SERVER['HTTP_REFERER']."\n" : '';
-  $message = 'URL: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n".$ref."$message";
+  $ua = 'User Agent: '.$_SERVER['HTTP_USER_AGENT']."\n";
+  $message = 'URL: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n".$ref.$ua."$message";
   mail('tom.kay@utopiasystems.co.uk',$subject,$message);
 }
 ?>
