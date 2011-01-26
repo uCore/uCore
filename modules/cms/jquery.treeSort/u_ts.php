f2598c200656698cab71d46e82266101899c9ba7
Tom Kay <oridan82@gmail.com>
2011-01-26 22:31:40 +0000

    fix include paths for jquery.treeSort

--- modules/cms/jquery.treeSort/u_ts.php
+++ modules/cms/jquery.treeSort/u_ts.php
@@ -5,8 +5,8 @@ class treeSort {
 		if (self::$init) return;
 		self::$init = true;
 
-		utopia::AddJSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.treeSort/jquery.treeSort.js'));
-		utopia::AddCSSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.treeSort/jquery.treeSort.css'));
+		utopia::AddJSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.treeSort.js'));
+		utopia::AddCSSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.treeSort.css'));
 	}
 }
 ?>
