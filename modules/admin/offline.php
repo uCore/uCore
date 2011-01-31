6c39a56ddfbe60029290af15c80eff9e385fdb98
Tom Kay <oridan82@gmail.com>
2011-01-31 22:17:14 +0000

    use modOpts to disable site

--- modules/admin/offline.php
+++ modules/admin/offline.php
@@ -3,16 +3,16 @@
 class module_Offline extends uBasicModule {
 	// title: the title of this page, to appear in header box and navigation
 	public function GetTitle() { return 'Site Offline'; }
-	public function GetOptions() { return DEFAULT_OPTIONS; }
 
 	public function SetupParents() {
-		if (internalmodule_AdminLogin::IsLoggedIn()) return;
+		modOpts::AddOption('module_Offline','online','Site Online',0,itYESNO);
+//		if (internalmodule_AdminLogin::IsLoggedIn()) return;
 		$this->AddParent('/');
 	}
 
 	public function ParentLoadPoint() { return 0; }
 	public function ParentLoad($parent) {
-		if (!$this->IsActive()) return true;
+		if (modOpts::GetOption('module_Offline','online')) return;
 		if (internalmodule_AdminLogin::IsLoggedIn()) return;
 		if (flag_is_set(CallModuleFunc($parent,'GetOptions'),IS_ADMIN)) return;
 
@@ -22,7 +22,7 @@ class module_Offline extends uBasicModule {
 	}
 
 	public function RunModule() {
-		echo '<h1>Site is currently offline.</h1>';
+		echo '<h1>Site is offline for maintenance.</h1>';
 	}
 }
 
