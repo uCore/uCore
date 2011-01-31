44e808babf21b1b444c3cba2860b575686df4e4a
Tom Kay <oridan82@gmail.com>
2011-01-31 16:52:15 +0000

    fix FlexDB references

--- modules/fancybox/fancybox.php
+++ modules/fancybox/fancybox.php
@@ -1,11 +1,11 @@
 <?php
 class fancybox {
 	public static function InitFancybox($mousewheel = false, $easing = false) {
-		$rs = FlexDB::GetRelativePath(dirname(__FILE__).'/jquery.fancybox-1.3.0.css');
-		$rj = FlexDB::GetRelativePath(dirname(__FILE__).'/jquery.fancybox-1.3.0.pack.js');
+		$rs = utopia::GetRelativePath(dirname(__FILE__).'/jquery.fancybox-1.3.0.css');
+		$rj = utopia::GetRelativePath(dirname(__FILE__).'/jquery.fancybox-1.3.0.pack.js');
 
-		FlexDB::AddCSSFile($rs);
-		FlexDB::AddJSFile($rj);
+		utopia::AddCSSFile($rs);
+		utopia::AddJSFile($rj);
 		if ($mousewheel) FlexDB::AddJSFile(FlexDB::GetRelativePath(dirname(__FILE__).'/jquery.mousewheel-3.0.2.pack.js'));
 		if ($easing) FlexDB::AddJSFile(lexDB::GetRelativePath(dirname(__FILE__).'/jquery.easing-1.3.pack.js'));
 	}
