2c9540171f845355c4572f6b7e768a4cede4aed7
Tom Kay <oridan82@gmail.com>
2011-02-08 15:00:12 +0000

    make plupload module completely independent, allow other modules to create options var using uPlupload::init

--- modules/plupload/plupload.php
+++ modules/plupload/plupload.php
@@ -1,27 +1,22 @@
 <?php
-class plupload extends uBasicModule {
-	function SetupParents() {
+class uPlupload {
+	static function Init($jsVarName,$uploadPath) {
 		utopia::AddJSFile(utopia::GetRelativePath(dirname(__FILE__).'/plupload.full.min.js'));
 		utopia::AddJSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.ui.plupload.min.js'));
 		utopia::AddCSSFile(utopia::GetRelativePath(dirname(__FILE__).'/jquery.ui.plupload.css'));
 
-		$pathUpload = CallModuleFunc('fileManager','GetAjaxUploadPath');
 		$pathCore = PATH_REL_CORE;
-		uJavascript::IncludeText(<<<FIN
-var pluploadOptions = {
+		uJavascript::AddText(<<<FIN
+var $jsVarName = {
     runtimes : 'gears,html5,flash,silverlight,browserplus,html4',
     chunk_size : '1mb',
-    //resize : {width : 640, height : 480, quality : 90},
-    url : '$pathUpload',
+    url : '$uploadPath',
     flash_swf_url : '{$pathCore}modules/plupload/plupload.flash.swf',
-    silverlight_xap_url : '{$pathCore}modules/plupload/plupload.silverlight.xap',
-    rename:true
+    silverlight_xap_url : '{$pathCore}modules/plupload/plupload.silverlight.xap'
 };
 FIN
 		);
 	}
-	function ParentLoad($parent) {}
-	function RunModule() {}
 }
 
 ?>
