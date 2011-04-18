1485a907ec07aed10dcd30fe470901897028d4b9
Tom Kay <oridan82@gmail.com>
2011-04-18 13:19:39 +0100

    more sensible check for running admin module

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once('start.php');
-if ($_SERVER['REQUEST_URI'] == PATH_REL_CORE || $_SERVER['REQUEST_URI'] == CallModuleFunc('internalmodule_Admin','GetURL',$_GET))
-	return RunModule('internalmodule_Admin');
-RunModule();
+if (!isset($_GET['uuid']))
+	RunModule('internalmodule_Admin');
+else
+	RunModule();
 ?>
