abe79b4ff976ed72f0d0c5134cbe4f052a2753f6
Tom Kay <oridan82@gmail.com>
2011-08-19 15:23:41 +0100

    fix bug with nested page list

--- modules/cms/cms.php
+++ modules/cms/cms.php
@@ -111,12 +111,11 @@ class uCMS_List extends uDataModule implements iAdminModule {
 			$('.ui-treesort-item:not(.ui-treesort-folder) > .cmsParentToggle').remove();
 			$('.ui-treesort-folder').each(function () {
 				var icon = $('.cmsParentToggle',this);
-				if (!icon.length) icon = $('<span class="cmsParentToggle ui-widget ui-icon" style="width:16px; float:left"></span>');
+				if (!icon.length) icon = $('<span class="cmsParentToggle ui-widget ui-icon" style="width:16px; float:left"></span>').prependTo(this);
 				if ($('ul:visible',this).length)
 					icon.removeClass('ui-icon-triangle-1-e').addClass('ui-icon-triangle-1-s');
 				else
 					icon.removeClass('ui-icon-triangle-1-s').addClass('ui-icon-triangle-1-e');
-				$(this).prepend(icon);
 			});
 		}
 		function dropped() {
