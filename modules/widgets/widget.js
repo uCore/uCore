3f70d8ab1519534005d1991922fc7fc5afb43638
Tom Kay <oridan82@gmail.com>
2011-11-23 14:55:57 +0000

    greatly improve widget insertion dialog

--- modules/widgets/widget.js
+++ modules/widgets/widget.js
@@ -1,22 +1,42 @@
 var chooseWidgetDialog = null;
 function ChooseWidget() {
 	if (!chooseWidgetDialog) {
-		chooseWidgetDialog = $('<div><p>Create a <a style="font-weight:bold" class="newWidgetLink" href="">new widget</a> or select an existing widget from the list below.</p><div class="widgets"></div></div>');
-		var ew = $('.widgets',chooseWidgetDialog);
+		chooseWidgetDialog = $('<div><p>Create a <a style="font-weight:bold" class="newWidgetLink" href="">new widget</a> or select an existing widget from the list below.</p><div class="widgets"></div><div id="widgetPreview" style="padding:10px"></div></div>');
+		var $ew = $('.widgets',chooseWidgetDialog);
+		// select box exists? - onchange, show preview
+		if (!$('#widgetSelector',$ew).length) $ew.prepend('<select id="widgetSelector"><option></option></select>');
+		var $wSel = $('#widgetSelector',$ew).change(function() {
+			$.get('?__ajax=getParserContent&ident=widget&data='+$(this).val(),function (data) {
+				$('#widgetPreview').html('<p style="font-weight:bold">Preview</p>'+data);
+			});
+		});
+
 		$.getJSON('?__ajax=getWidgets',function (data) {
 			$('.newWidgetLink',chooseWidgetDialog).attr('href',data[0]);
 			data = data[1];
 			var cType = null;
+
 			$(data).each(function (i) {
 				if (!data[i]['block_type']) return;
-				if (!cType || cType != data[i]['block_type']) { cType = data[i]['block_type']; ew.append('<h2>'+cType+'</h2>'); }
-				var btn = $('<span>'+data[i]['block_id']+'</span>').button();
-				btn.click(function () { InsertWidget(data[i]['block_id']); chooseWidgetDialog.dialog('close'); });
-				ew.append(btn);
+				if (!cType || cType != data[i]['block_type']) { cType = data[i]['block_type']; $wSel.append('<optgroup label="'+cType+'" />'); }
+				var $grp = $('optgroup[label="'+cType+'"]',$wSel);
+				$grp.append('<option>'+data[i]['block_id']+'</option');
 			});
 		});
-		chooseWidgetDialog.dialog({autoOpen:false, modal:true});
+		chooseWidgetDialog.dialog({autoOpen:false, modal:true,buttons: {
+			"Insert Widget": function() {
+				InsertWidget($wSel.val());
+				$( this ).dialog( "close" );
+			},
+			Cancel: function() {
+				$( this ).dialog( "close" );
+			}
+		}});
 	}
+
+	// reset view
+	$('#widgetPreview').html('');
+	$('#widgetSelector').val('');
 
 	// open widget selection
 	chooseWidgetDialog.dialog('open');
