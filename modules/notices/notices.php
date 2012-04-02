231de498e0f6204f10107957b4420a4e17db9a36
Tom Kay <oridan82@gmail.com>
2012-04-02 15:35:12 +0000

    output notices as alerts if no notice container is found

--- modules/notices/notices.php
+++ modules/notices/notices.php
@@ -11,7 +11,7 @@ class uNotices extends uBasicModule {
 	}
 
 	public static function AddNotice($message,$type=NOTICE_TYPE_INFO) {
-		if (!AjaxEcho('$(\'.uNotices\').append(Base64.decode("'.base64_encode(self::GetNotice($message,$type)).'"));'))
+		if (!AjaxEcho('var msg = Base64.decode("'.base64_encode(self::GetNotice($message,$type)).'"); if ($(\'.uNotices\').length) $(\'.uNotices\').append(msg); else alert(Base64.decode("'.base64_encode($message).'"));'))
 			$_SESSION['notices'][] = array('message'=>$message, 'type'=>$type);
 	}
 
