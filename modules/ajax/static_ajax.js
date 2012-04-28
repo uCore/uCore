bb8cbabb9a8975a1c99e536b050bf12eddb257a8
Tom Kay <oridan82@gmail.com>
2012-04-28 21:11:30 +0000

    include hidden submit button on all forms. ensures users are able to hit enter to submit forms.

--- modules/ajax/static_ajax.js
+++ modules/ajax/static_ajax.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 	$("<img>").attr("src", PATH_REL_CORE+'images/hourglass.png');
 	//$("<img>").attr("src", PATH_REL_CORE+'images/utopia-systems-hover.png');
 	
-	if (typeof(DD_belatedPNG) != 'undefined') DD_belatedPNG.fix('*');
+	$('form').append('<input type="submit" style="width:0;height:0;border:0;padding:0;margin:0;" />');
 
 	$('li:first','ul').addClass('first-item');
 	$('li:last','ul').addClass('last-item');
