4167bde32f45948f48ee27f39a25556b40f981fa
Tom Kay <oridan82@gmail.com>
2012-05-04 15:49:57 +0000

    fix 'cannot redefine function' error

--- classes/static.class.php
+++ classes/static.class.php
@@ -1031,23 +1031,25 @@ class utopia {
 	}
 
 	static function GetMaxUpload($post=true,$upload_max=true) {
-		function let_to_num($v){ //This function transforms the php.ini notation for numbers (like '2M') to an integer (2*1024*1024 in this case)
-		    $l = substr($v, -1);
-		    $ret = substr($v, 0, -1);
-		    switch(strtoupper($l)){
-		    case 'P':
-		        $ret *= 1024;
-		    case 'T':
-		        $ret *= 1024;
-		    case 'G':
-		        $ret *= 1024;
-		    case 'M':
-		        $ret *= 1024;
-		    case 'K':
-		        $ret *= 1024;
-		        break;
-		    }
-		    return $ret;
+		if (!function_exists('let_to_num')){
+			function let_to_num($v){ //This function transforms the php.ini notation for numbers (like '2M') to an integer (2*1024*1024 in this case)
+				$l = substr($v, -1);
+				$ret = substr($v, 0, -1);
+				switch(strtoupper($l)){
+				case 'P':
+					$ret *= 1024;
+				case 'T':
+					$ret *= 1024;
+				case 'G':
+					$ret *= 1024;
+				case 'M':
+					$ret *= 1024;
+				case 'K':
+					$ret *= 1024;
+					break;
+				}
+				return $ret;
+			}
 		}
 		if ($post) $post = let_to_num(ini_get('post_max_size')); else $post = NULL;
 		if ($upload_max) $upload_max = let_to_num(ini_get('upload_max_filesize')); else $upload_max = NULL;
