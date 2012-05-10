858c9be2673f80ae4b4f6f7508c106054e143deb
Tom Kay <oridan82@gmail.com>
2012-05-10 20:54:02 +0000

    also assert the template if current module isnt the cms

--- modules/cms/cms.php
+++ modules/cms/cms.php
@@ -484,7 +484,7 @@ class uCMS_View extends uSingleDataModule {
 		$page = self::findPage();
 		return $page['updated'];
 	}
-	public function assertContent() { if (utopia::GetCurrentModule() !== __CLASS__ && self::findPage()) echo '{content}'; }
+	public function assertContent() { if (utopia::GetCurrentModule() !== __CLASS__ && $rec = self::findPage()) { echo '{content}'; utopia::UseTemplate(self::GetTemplate($rec['cms_id'])); } }
 	static function templateParser($id) {
 		$obj = utopia::GetInstance('uCMS_View');
 		$rec = $obj->LookupRecord($id);
