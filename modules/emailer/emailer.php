ab358bed8c7c0fb8cc11f887e01a125fc265bcf3
Tom Kay <oridan82@gmail.com>
2012-06-15 12:12:43 +0000

    provide uEmailer::IsEmail helper function

--- modules/emailer/emailer.php
+++ modules/emailer/emailer.php
@@ -249,5 +249,7 @@ class uEmailer extends uDataModule {
 		}
 		return $output;
 	}
+	public static function IsEmail($email) {
+		return (bool)preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i',$email);
+	}
 }
-
