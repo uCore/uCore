a5049057cb91ea083d338128e94e8abf42bf34f0
Tom Kay <oridan82@gmail.com>
2012-10-10 11:37:53 +0000

    bugfix re-initialisation of widgets when drawing from data

--- modules/widgets/widget.php
+++ modules/widgets/widget.php
@@ -108,17 +108,19 @@ class uWidgets extends uSingleDataModule implements iAdminModule {
 	}
 
 	static function DrawWidget($rec) {
+		$obj = utopia::GetInstance('uWidgets',false);
 		if (!is_array($rec)) {
 			if (self::StaticWidgetExists($rec)) return call_user_func(self::$staticWidgets[$rec]);
 
-			$obj = utopia::GetInstance('uWidgets');
 			$rec = $obj->LookupRecord($rec);
 			if (!$rec) return '';
-			$obj->InitInstance($rec['block_type']); // init and re-request
-			$rec = $obj->LookupRecord($rec['block_id']);
 		}
 		$content = '';
-		if ($rec['block_type'] && class_exists($rec['block_type'])) $content = call_user_func(array($rec['block_type'],'DrawData'),$rec);
+		if ($rec['block_type'] && class_exists($rec['block_type'])){
+			$obj->InitInstance($rec['block_type']); // init and re-request
+			$rec = $obj->LookupRecord($rec['block_id']);
+			$content = call_user_func(array($rec['block_type'],'DrawData'),$rec);
+		}
 
 		$ret = '<div class="uWidget uWidget-'.$rec['block_id'].'">'.$content.'</div>';
 
