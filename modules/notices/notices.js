8a5ee428eb0f79da4155f82ab63cd6520855703d
Tom Kay <oridan82@gmail.com>
2013-01-01 22:01:42 +0000

    fix notices when notice is not triggered via ajax. implement reading speed

--- modules/notices/notices.js
+++ modules/notices/notices.js
@@ -3,5 +3,9 @@ utopia.ShowNotice = function (msg) {
 	if (!$('.uNotices').length) $('body').append('<div class="uNotices-wrap"><div class="uNotices"/></div>');
 	$msg = $(msg).hide();
 	$('.uNotices').append($msg);
-	$msg.slideDown(500).delay(2500).slideUp(500,function(){console.log(this);$(this).remove()});
+
+	var words = $msg.text().match(/\w+/g).length;
+	var ms = (words / 180) * 60 * 1000; // 180 words per minute
+	
+	$msg.slideDown(500).delay(1000+ms).slideUp(500,function(){console.log(this);$(this).remove()});
 }
--- modules/notices/notices.php
+++ modules/notices/notices.php
@@ -4,23 +4,14 @@ define('NOTICE_TYPE_INFO'	, 'info');
 define('NOTICE_TYPE_WARNING', 'warning');
 define('NOTICE_TYPE_ERROR'	, 'error');
 
-class uNotices extends uBasicModule {
-	public function SetupParents() {
-		uCSS::IncludeFile(dirname(__FILE__).'/notices.css');
-		uJavascript::IncludeFile(dirname(__FILE__).'/notices.js');
-	}
-
+class uNotices {
 	public static function AddNotice($message,$type=NOTICE_TYPE_INFO) {
-		if (!AjaxEcho('var msg = Base64.decode("'.base64_encode(self::GetNotice($message,$type)).'"); utopia.ShowNotice(msg);'))
-			$_SESSION['notices'][] = array('message'=>$message, 'type'=>$type);
+		$script = 'var msg = Base64.decode("'.base64_encode(self::GetNotice($message,$type)).'"); utopia.ShowNotice(msg);';
+		if (!AjaxEcho($script)) $_SESSION['notices'][] = $script;
 	}
-
 	public static function GetNotice($message,$type=NOTICE_TYPE_INFO) {
 		$icon = $type === NOTICE_TYPE_WARNING || $type === NOTICE_TYPE_ERROR ? 'alert' : 'info';
 		return '<div class="uNotice uNotice-'.$type.'">'.$message.'</div>';
-	}
-	public function RunModule() {
-		self::ShowNotices();
 	}
 	public static function ShowNotices() {
 		// is redirect issued?  If so, don't draw now.
@@ -28,11 +19,15 @@ class uNotices extends uBasicModule {
 			if (preg_match('/^location:/i',$h)) return;
 		}
 		
-		echo '<div class="uNotices">';
-		if (isset($_SESSION['notices'])) foreach ($_SESSION['notices'] as $k => $notice) {
-			echo self::GetNotice($notice['message'],$notice['type']);
-			unset($_SESSION['notices'][$k]);
-		}
-		echo '</div>';
+		$scripts = '$(function(){'.implode(PHP_EOL,$_SESSION['notices']).'});';
+		$_SESSION['notices'] = array();
+		uJavascript::AddText($scripts);
+	}
+	public static function Init() {
+		uCSS::IncludeFile(dirname(__FILE__).'/notices.css');
+		uJavascript::IncludeFile(dirname(__FILE__).'/notices.js');
 	}
 }
+
+uEvents::AddCallback('AfterInit','uNotices::Init');
+uEvents::AddCallback('BeforeOutputTemplate','uNotices::ShowNotices');
