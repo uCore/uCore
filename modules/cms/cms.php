7592fd4cc37d3ebf877e8b651ba0b123eafa09af
Tom Kay <oridan82@gmail.com>
2013-01-30 17:32:03 +0000

    temporarily disable processWidget callback (intended for use in editor only)

--- modules/cms/cms.php
+++ modules/cms/cms.php
@@ -226,7 +226,7 @@ class uCMS_Edit extends uSingleDataModule implements iAdminModule {
 		$this->AddField('publishing',array($this,'publishLinks'),'cms','Publish');
 
 		$this->AddField('content','content','cms','Page Content',itHTML);
-		$this->AddPreProcessCallback('content',array($this,'processWidget'));
+//		$this->AddPreProcessCallback('content',array($this,'processWidget'));
 		$this->FieldStyles_Set('content',array('width'=>'100%'));
 		$this->AddField('content_published','content_published','cms');
 
