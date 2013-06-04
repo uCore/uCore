78625272a1fbc3024db5eac21e36e648b01df582
Tom Kay <oridan82@gmail.com>
2013-06-04 12:40:22 +0100

    improve DataOnly module to only allow specified modules

--- modules/dataonly/dataonly.php
+++ modules/dataonly/dataonly.php
@@ -1,13 +1,24 @@
 <?php
 
-utopia::RegisterAjax('csv','uDataOnly::csv');
-utopia::RegisterAjax('raw','uDataOnly::rawOutput');
-class uDataOnly  {
-	public static function inject($module) {
-		uEvents::AddCallback('BeforeRunModule','uDataOnly::inject_run',$module);
+utopia::RegisterAjax('dataonly','uDataOnly::process');
+uDataOnly::RegisterType('csv','uDataOnly::csv');
+uDataOnly::RegisterType('json','uDataOnly::json');
+uEvents::AddCallback('AfterInit','uDataOnly::processInjectionQueue',null,MAX_ORDER);
+class uDataOnly {
+	private static $types = array();
+	public static function RegisterType($type,$callback) {
+		self::$types[$type] = $callback;
 	}
-
-	public static function inject_run($obj) {
+	private static $queue = array();
+	public static function inject($module) {
+		self::$queue[] = $module;
+	}
+	public static function processInjectionQueue() {
+		foreach (self::$queue as $module) {
+			self::doInject($module);
+		}
+	}
+	private static function doInject($obj) {
 		if (is_string($obj)) {
 			$parent = $obj;
 			$obj =& utopia::GetInstance($parent);
@@ -15,28 +26,48 @@ class uDataOnly  {
 			$parent = get_class($obj);
 		}
 		if (!is_subclass_of($parent,'uDataModule')) return;
-		$url = $obj->GetURL(array_merge($_GET,array('__ajax'=>'csv')));
+		$url = $obj->GetURL(array_merge($_GET,array('__ajax'=>'dataonly','dataonly-type'=>'csv')));
 		utopia::LinkList_Add('list_functions:'.$parent,'Export to CSV',$url,10,NULL,array('class'=>'btn btn-csv'));
+		self::$allowed[] = $parent;
+	}
+	private static $allowed = array();
+	private static function IsAllowed($module) {
+		return (array_search($module,self::$allowed) !== FALSE);
+	}
+	public static function process() {
+		$type = isset($_GET['dataonly-type']) ? $_GET['dataonly-type'] : NULL;
+		if (!$type || !isset(self::$types[$type])) {
+			utopia::UseTemplate();
+			utopia::PageNotFound(); return;
+		}
+		
+		$qs = $_GET;
+		unset($qs['__ajax']);
+		unset($qs['dataonly-type']);
+		
+		$cm = utopia::GetCurrentModule();
+		if (!self::IsAllowed($cm)) {
+			// redirect to module
+			$obj =& utopia::GetInstance($cm);
+			header('Location: '.$obj->GetURL($qs)); return;
+		}
+		
+		try { // try to access it dataonly, if it fails for any reason, break out of , redirect
+			call_user_func(self::$types[$type]);
+		} catch (Exception $e) {
+			$obj =& utopia::GetInstance($cm);
+			header('Location: '.$obj->GetURL($qs)); return;
+		}
 	}
 	
-	public static function rawOutput() {
-		$type = 'json';
-		if (array_key_exists('_type',$_GET))
-			$type = $_GET['_type'];
-		
-		if (!array_key_exists('_expires',$_GET) || !$_GET['_expires']) $_GET['_expires'] = 60*60*24*1;
-		
+	public static function json() {
 		$etag = sha1($_SERVER['REQUEST_URI']);
 		
-		utopia::Cache_Check($etag,'application/json','',NULL,$_GET['_expires']);
-		switch ($type) {
-			case 'json':
-				$obj =& utopia::GetInstance(utopia::GetCurrentModule());
-				$data = json_encode($obj->GetRawData());
-				utopia::Cache_Output($data,$etag,'application/json','',NULL,$_GET['_expires']);
-//				die();
-			break;
-		}
+		utopia::Cache_Check($etag,'application/json');
+
+		$obj =& utopia::GetInstance(utopia::GetCurrentModule());
+		$data = json_encode($obj->GetRawData());
+		utopia::Cache_Output($data,$etag,'application/json');
 	}
 
 	public static function csv() {
