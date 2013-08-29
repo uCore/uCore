7d1c615f86020a716d1012a4c61128cb6d602d8c
Tom Kay <oridan82@gmail.com>
2013-08-29 15:30:19 +0100

    remove obsolete javascript

--- modules/javascript/javascript.php
+++ modules/javascript/javascript.php
@@ -146,8 +146,3 @@ class uJavascript extends uBasicModule {
 uJavascript::LinkFile(dirname(__FILE__).'/jQuery/jquery-1.10.2.min.js',-100);
 uJavascript::LinkFile(dirname(__FILE__).'/jQuery/jquery-ui-1.10.0.min.js',-99);
 uJavascript::IncludeFile(dirname(__FILE__).'/javascript.js',-999);
-uJavascript::IncludeFile(dirname(__FILE__).'/js/min/jquery.metadata.min.js');
-uJavascript::IncludeFile(dirname(__FILE__).'/carousel/jquery.jcarousel.min.js');
-uJavascript::IncludeFile(dirname(__FILE__).'/js/ajaxfileupload.js');
-uJavascript::IncludeFile(dirname(__FILE__).'/js/sqlDate.js');
-uJavascript::IncludeFile(dirname(__FILE__).'/js/functs.js');
