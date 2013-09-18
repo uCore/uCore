ced16c7f172b5d6c2a91800de4edff53ba45507a
Tom Kay <oridan82@gmail.com>
2013-09-18 09:21:23 +0000

    remove finalise.php and put code in Finish

--- classes/static.class.php
+++ classes/static.class.php
@@ -270,7 +270,22 @@ class utopia {
 		if (self::$finished) return;
 		self::$finished = true;
 		while (ob_get_level() > 3) ob_end_flush();
-		include(PATH_ABS_CORE.'finalise.php');
+		
+		timer_start('Output Template');
+		utopia::OutputTemplate();
+		timer_end('Output Template');
+
+		if (isset($GLOBALS['timers']) && utopia::DebugMode()) {
+			echo '<pre class="uDebug"><table>';
+			foreach ($GLOBALS['timers'] as $name => $info) {
+				if (!is_array($info)) continue;
+				$time = !array_key_exists('time_taken',$info) ? timer_end($name) : $info['time_taken'];
+				echo '<tr><td style="vertical-align:top;border-top:1px solid black">'.$time.'</td><td style="vertical-align:top;border-top:1px solid black">'.$name.PHP_EOL.$info['info'].'</td></tr>';
+			}
+			echo '</table></pre>';
+		}
+
+		header('X-Runtime: '.number_format((microtime(true)-UCORE_START_TIME)*1000).'ms');
 	}
 
 	private static $customInputs = array();
