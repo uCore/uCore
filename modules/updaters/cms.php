02abd154e8185e65d3b89c33c2d8a1f457b183cd
Tom Kay <oridan82@gmail.com>
2013-10-19 14:17:22 +0000

    optimise publish time updater

--- modules/updaters/cms.php
+++ modules/updaters/cms.php
@@ -1,13 +1,22 @@
 <?php
 
-function uCMS_publish_update() {
-	$o = utopia::GetInstance('uCMS_Edit');
-	$o->BypassSecurity(true);
-	$ds = $o->GetDataset(array('{content_published_time} > {content_time}'));
-	while (($row = $ds->fetch())) {
-		$o->UpdateFieldRaw('content_published_time','`content_time`',$row['cms_id']);
+class cms_publish_update implements iUtopiaModule {
+	public static function Initialise() {
+		uEvents::AddCallback('AfterInit','cms_publish_update::uCMS_publish_update');
+	
 	}
-	$o->BypassSecurity(false);
+	public static function uCMS_publish_update() {
+		$done = modOpts::GetOption('cms_publish_update');
+		if ($done) return;
+	
+		$o = utopia::GetInstance('uCMS_Edit');
+		$o->BypassSecurity(true);
+		$ds = $o->GetDataset(array('{content_published_time} > {content_time}'));
+		while (($row = $ds->fetch())) {
+			$o->UpdateFieldRaw('content_published_time','`content_time`',$row['cms_id']);
+		}
+		$o->BypassSecurity(false);
+		
+		modOpts::SetOption('cms_publish_update',true);
+	}
 }
-
-uEvents::AddCallback('AfterInit','uCMS_publish_update');
